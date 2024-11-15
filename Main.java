60ff14ebbab0281d01a557e921c3af50baf84531
JuGerez <100386098+JuGerez@users.noreply.github.com>
2024-11-14 23:41:15 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Menu.MenuGral;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        MenuGral.menuPrincipal();
+    }
+}
